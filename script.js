d391c478c8d6c6ffed7b196b7316fed1d61bc519
Massaru <massaruhagiopro@gmail.com>
2021-10-01 03:41:12 +0900

    update

--- script.js
+++ script.js
@@ -1,49 +1,36 @@
-var listageral = []; // preparando para receber dados
+var listaStorage = localStorage.getItem("listageral");
+var listageral =
+    listaStorage == null || listaStorage == "[]"
+        ? (listageral = [])
+        : (listageral = JSON.parse(localStorage.getItem("listageral"))); // preparando para receber dados
 
 function addlista() {
     let valor = document.getElementById("entrada").value;
     listageral.push(valor);
-    return listageral;
+    salvar();
+    location.reload();
 }
 function salvar() {
-    let texto = listageral; //
     console.log(JSON.stringify(listageral)); // um loge para ver o que esta acontecendo
     localStorage.setItem("listageral", JSON.stringify(listageral)); // uso json para salvar no local storage
 }
 function limpar() {
     localStorage.clear();
-    alert("Cache Limpo");
     location.reload();
 }
-function visualizar() {
-    alert("lista " + listageral);
-    alert("lista gravada " + localStorage.getItem("listageral"));
-}
-function excluir() {
-    console.log("teste");
+function excluir(elemento) {
+    let delelemento = elemento.getAttribute("value");
+    console.log(delelemento);
+    listageral.splice(delelemento, 1);
+    salvar();
+    location.reload();
 }
 
 // design
-
-function designListaGravada() {
-    // serve para acrescentar listageral do localStorage no html
-    let conteudo = document.getElementById("result");
-    let listagravada = JSON.parse(localStorage.getItem("listageral"));
-    let strchar = "";
-    if (listagravada != null) {
-        for (var i of listagravada) {
-            strchar +=
-                "<li class='dados'>" +
-                i +
-                "</li><input type='button' class='caixa' value='excluir'><br>";
-        }
-    }
-    conteudo.innerHTML = strchar;
-}
 function designListaGeral() {
     // serve para acrescentar listageral no html
     let conteudo = document.getElementById("result");
-    let listagravada = listageral;
+    let listagravada = JSON.parse();
     let strchar = "";
     let num = 0;
     if (listagravada != null) {
@@ -51,11 +38,12 @@ function designListaGeral() {
             strchar +=
                 "<li class='dados'>" +
                 i +
-                "</li><img class='imagem' src='img/del.png' alt='" +
+                "</li><img class='imagem' src='img/del.png' value='" +
                 num +
-                "' onclick='excluir()'><br>";
+                "' onclick='excluir(this)'><br>";
             num++;
         }
     }
     conteudo.innerHTML = strchar;
+    // location.reload();
 }
